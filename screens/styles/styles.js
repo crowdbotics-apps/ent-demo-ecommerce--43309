ce649322b9ab6f0a0b6d0613ab93856fbba0ee04
cb-kozel <132781984+cb-kozel@users.noreply.github.com>
2023-09-09 19:01:56 -0700

    added styles

---
+++ screens/styles/styles.js
@@ -0,0 +1,230 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  addToBag: {
+    width: "50%",
+    minHeight: 48,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000",
+    marginTop: 15,
+    marginBottom: 15,
+    fontFamily: "Helvetica"
+  },
+  addToBagText: {
+    color: "#fff"
+  },
+  bag: {
+    position: "absolute",
+    right: 50,
+    top: 10,
+    width: 25,
+    height: 25,
+    resizeMode: "cover",
+    marginBottom: 8
+  },
+  cardContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    flex: 1 / 5,
+    padding: 16,
+    marginLeft: 10,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#f4f4f4",
+    padding: 16,
+    margin: 30
+  },
+  description: {
+    fontFamily: "Helvetica",
+    fontSize: 16,
+    marginTop: 12
+  },
+  flexColumn1: {
+    flex: 1 / 3,
+    flexDirection: "column",
+    marginLeft: 30
+  },
+  flexColumn2: {
+    flex: 1 / 2,
+    flexDirection: "column"
+  },
+  flexRow: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  h1: {
+    fontFamily: "Helvetica",
+    fontWeight: "bold",
+    fonSize: "5em",
+    marginBottom: 12
+  },
+  h2: {
+    fontFamily: "Helvetica",
+    fontWeight: "bold",
+    fonSize: 16,
+    marginBottom: 12
+  },
+  header: {
+    height: 60,
+    paddingLeft: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  headerImage: {
+    width: "77px",
+    height: "32px",
+    resizeMode: "cover",
+    marginBottom: 8
+  },
+  links: {
+    fontFamily: "Helvetica",
+    fontWeight: 700,
+    marginRight: "1em"
+  },
+  material: {
+    fontFamily: "Helvetica",
+    fontSize: 14,
+    marginTop: 8
+  },
+  materialTitle: {
+    fontFamily: "Helvetica",
+    fontSize: 14,
+    marginTop: 12,
+    fontWeight: "bold"
+  },
+  pdpImage: {
+    maxWidth: 400,
+    minHeight: 350,
+    resizeMode: "cover",
+    marginBottom: 8
+  },
+  pdpImagePr: {
+    flex: 1,
+    minWidth: 80,
+    minHeight: 80,
+    resizeMode: "cover",
+    marginBottom: 8,
+    marginRight: 10
+  },
+  pdpImageSmWr: {
+    flex: 1,
+    width: "100%",
+    height: 200,
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  pdpTitle: {
+    fontFamily: "Helvetica",
+    fontWeight: 700,
+    fontSize: "2em",
+    marginLeft: 30
+  },
+  prevProductPrice: {
+    fontFamily: "Helvetica",
+    fontSize: 16,
+    color: "#888",
+    fontFamily: "sans-serif",
+    textDecorationLine: "line-through"
+  },
+  productImage: {
+    minWidth: 30,
+    minHeight: 150,
+    resizeMode: "cover",
+    marginBottom: 8
+  },
+  productPrice: {
+    fontFamily: "Helvetica",
+    fontSize: 20,
+    color: "#888"
+  },
+  productPriceSm: {
+    fontFamily: "Helvetica",
+    fontSize: 10,
+    color: "#888"
+  },
+  productTitleSm: {
+    fontSize: 12,
+    fontWeight: "bold",
+    fontFamily: "Helvetica",
+    marginBottom: 4
+  },
+  productTitleLg: {
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: "Helvetica",
+    marginBottom: 4
+  },
+  removeFilt: {
+    flex: 1,
+    width: "1em",
+    height: "1em",
+    resizeMode: "contain"
+  },
+  reset: {
+    backgroundColor: "#ffffff",
+    padding: 5,
+    borderRadius: 5
+  },
+  resetText: {
+    marginLeft: 20
+  },
+  reset: {
+    backgroundColor: "#ffffff",
+    padding: 5,
+    borderRadius: 5
+  },
+  resetText: {
+    marginLeft: 20
+  },
+  removeFilt: {
+    flex: 1,
+    width: "1em",
+    height: "1em",
+    resizeMode: "contain"
+  },
+  subHeader: {
+    alignItems: "center",
+    height: 40,
+    marginBottom: 10,
+    paddingLeft: 20,
+    flexDirection: "row",
+    width: "40%"
+  },
+  subHeaderText: {
+    color: "#c41320",
+    fontFamily: "Helvetica",
+    fontWeight: "bold",
+    fontSize: "1.5em",
+    marginRight: "1.5em"
+  },
+  title: {
+    fontFamily: "Helvetica",
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  topNav: {
+    fontFamily: "Helvetica",
+    fontWeight: 700,
+    fontSize: "14px",
+    lineHeight: "24px",
+    letterSpacing: ".02em",
+    position: "relative",
+    marginLeft: "16px",
+    marginRight: "16px"
+  },
+  OKkUptWz: {
+    color: "#c41320"
+  } //Product Card
+
+});
